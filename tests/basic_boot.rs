86efe73d645213750153af845bf42db8caba29c5
iragut <74363823+iragut@users.noreply.github.com>
2025-06-19 03:34:41 +0300

    Finish tests

---
+++ tests/basic_boot.rs
@@ -0,0 +1,27 @@
+// in tests/basic_boot.rs
+
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(game_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use game_os::println;
+
+#[unsafe(no_mangle)] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    game_os::test_panic_handler(info)
+}
